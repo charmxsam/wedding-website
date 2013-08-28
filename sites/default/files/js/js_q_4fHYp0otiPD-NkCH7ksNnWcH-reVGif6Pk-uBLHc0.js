5b151607220d6007c47eb2c48d3887c2fb5eb9ca
Jason Blanchard <jas.blanchard@gmail.com>
2013-08-28 16:27:05 -0700

    Added google analytics and cache stuff

---
+++ sites/default/files/js/js_q_4fHYp0otiPD-NkCH7ksNnWcH-reVGif6Pk-uBLHc0.js
@@ -0,0 +1,25 @@
+(function($) {
+  Drupal.behaviors.mobileMenu = {
+    attach: function(context, settings) {
+
+      var button;
+      var menu = $('.block--menu-menu-mobile-menu ul');
+
+      $(menu).hide();
+
+      if ($('.mobile-button').length == 0) {
+
+        button = $('.l-branding').after("<div><a href='#' class='mobile-button pull-right'><span class='bar'></span><span class='bar'></span><span class='bar'></span></a><div>");
+
+        $('.mobile-button').click(function() {
+          menu.slideToggle('fast');
+
+          return false;
+
+      });
+      }
+
+    }
+  }
+})(jQuery);
+;
